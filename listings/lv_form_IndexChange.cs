0deaaa403ba70844fecc896cd66052a73c926783
Alexandra Neffgen <a.neffgen@me.com>
2016-06-22 22:48:03 +0200

    Workflow die erste Hälfte

--- listings/lv_form_IndexChange.cs
+++ listings/lv_form_IndexChange.cs
@@ -1,10 +1,8 @@
  private void lv_form_SelectedIndexChanged(object sender, EventArgs e)
  {
-    Model.Form selected = null;
-    ListView.SelectedListViewItemCollection selectedItem = 
-    lv_form.SelectedItems;
-      foreach (ListViewItem li in selectedItem)
-         selected = (Model.Form)li.Tag;
+    (...)
+     foreach (ListViewItem li in selectedItem)
+        selected = (Model.Form)li.Tag;
      studentList.Items.Clear();
      studentList.Columns.Clear();
      if (selected != null)
@@ -13,12 +11,11 @@
          studentList.Width - 20);
          foreach (Student s in selected.getStudents())
          {
-             Console.WriteLine(s.getSurname());
              ListViewItem stud = new ListViewItem();
              stud.Tag = s;
              stud.Text = s.getGivenname() + "," + s.getSurname();
              studentList.Items.Add(stud);
          }
      }
-     studentList.AutoResizeColumns(ColumnHeaderAutoResizeStyle.None);
+     (...)
  }
